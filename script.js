2cd3130f94f4bdf1c681874d639d9dc41fe17ba2
tpalmer111D <timpalmer@Tims-MacBook-Pro.local>
2022-08-19 22:10:37 -0600

    js updated, tallies each color

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ console.log("Hello, from inside mini-mandala")
 addEventListener("DOMContentLoaded", () => {
     const compScoreBoard = document.querySelector(".CPU-score")
     const fieldDiv = document.querySelector(".field")
-    const cardOne = document.querySelector(".card-one")
-    const cardTwo = document.querySelector(".card-two")
-    const cardThree = document.querySelector(".card-three")
-    const cardFour = document.querySelector(".card-four")
+    const cardOne = document.querySelector("#card-one")
+    const cardTwo = document.querySelector("#card-two")
+    const cardThree = document.querySelector("#card-three")
+    const cardFour = document.querySelector("#card-four")
+    const redBox = document.querySelector("#red-box")
+    const blueBox = document.querySelector("#blue-box")
+    const greenBox = document.querySelector("#green-box")
+    const yellowBox = document.querySelector("#yellow-box")
     const playerScoreBoard = document.querySelector(".p1-score")
 
 
@@ -42,6 +46,10 @@ const colorFillFour = () => {
     document.querySelector("#card-four").style.backgroundColor = colorPicker()
 }
 
+let redTally = 0
+let blueTally = 0
+let greenTally = 0
+let yellowTally = 0
 
 
 colorFillOne()
@@ -49,15 +57,112 @@ colorFillTwo()
 colorFillThree()
 colorFillFour()
 
+cardOne.addEventListener("click", () => {
+    let currentColor = cardOne.style.backgroundColor
+    if (currentColor == "red") {
+        redTally++
+        redBox.innerText = redTally
+    } else if (currentColor == "blue") {
+        blueTally++
+        blueBox.innerText = blueTally
+    } else if (currentColor == "green"){
+        greenTally++
+        greenBox.innerText = greenTally
+    } else if (currentColor = "yellow") {
+        yellowTally++
+        yellowBox.innerText = yellowTally
+    }
+
+    colorFillOne()
+
+    console.log("red = " + redTally)
+    console.log("blue = " + blueTally)
+    console.log("green + " + greenTally)
+    console.log("yellow = " + yellowTally)
+
+    
+
+})
+
+cardTwo.addEventListener("click", () => {
+    let currentColor = cardTwo.style.backgroundColor
+    if (currentColor == "red") {
+        redTally++
+        redBox.innerText = redTally
+    } else if (currentColor == "blue") {
+        blueTally++
+        blueBox.innerText = blueTally
+    } else if (currentColor == "green"){
+        greenTally++
+        greenBox.innerText = greenTally
+    } else if (currentColor = "yellow") {
+        yellowTally++
+        yellowBox.innerText = yellowTally
+    }
+
+    console.log("red = " + redTally)
+    console.log("blue = " + blueTally)
+    console.log("green = " + greenTally)
+    console.log("yellow = " + yellowTally)
+    
+    colorFillTwo()
+})
+
+cardThree.addEventListener("click", () => {
+    let currentColor = cardThree.style.backgroundColor
+    if (currentColor == "red") {
+        redTally++
+        redBox.innerText = redTally
+    } else if (currentColor == "blue") {
+        blueTally++
+        blueBox.innerText = blueTally
+    } else if (currentColor == "green"){
+        greenTally++
+        greenBox.innerText = greenTally
+    } else if (currentColor = "yellow") {
+        yellowTally++
+        yellowBox.innerText = yellowTally
+    }
+    
+    console.log("red = " + redTally)
+    console.log("blue = " + blueTally)
+    console.log("green + " + greenTally)
+    console.log("yellow = " + yellowTally)
+
+    colorFillThree()
+})
+
+cardFour.addEventListener("click", () => {
+    let currentColor = cardFour.style.backgroundColor
+    if (currentColor == "red") {
+        redTally++
+        redBox.innerText = redTally
+    } else if (currentColor == "blue") {
+        blueTally++
+        blueBox.innerText = blueTally
+    } else if (currentColor == "green"){
+        greenTally++
+        greenBox.innerText = greenTally
+    } else if (currentColor = "yellow") {
+        yellowTally++
+        yellowBox.innerText = yellowTally
+    }
+  
+    console.log("red = " + redTally)
+    console.log("blue = " + blueTally)
+    console.log("green + " + greenTally)
+    console.log("yellow = " + yellowTally)
+
+    colorFillFour()
+
+
 })
 
 
 
+})
 
-//  const pixels = document.querySelectorAll(".pixel")
-//  pixels.forEach(pixel => {
-//  pixel.addEventListener("mouseenter", () => {
-//  pixel.style.backgroundColor = "black"
-// })
+
+
 
 
